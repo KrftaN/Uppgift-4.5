0932d536c964269f19d16dd99053a25c68897ad5
KraftaN <63770425+KrftaN@users.noreply.github.com>
2023-10-22 12:02:39 +0200

    Add project files.

---
+++ Uppgift 4.5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Skriv in ett heltal");
+        string heltal = Console.ReadLine();
+        string talet = "";
+        for (int i = 0; i < heltal.Length; i++)
+        {
+            string tecken = heltal[i].ToString();
+            int ental = Convert.ToInt32((tecken));
+            ental++;
+            if (ental == 9)
+            {
+                ental = 0;
+            }
+
+            talet += ental;
+
+        }
+        Console.WriteLine(talet);
+
+    }
+}
